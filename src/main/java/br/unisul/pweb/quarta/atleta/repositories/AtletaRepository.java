f46cbd8849202932352a0e439eafe02668c3df37
biel12321 <gabriellw.grb@gmail.com>
2019-04-10 20:33:34 -0300

    Adicionado Anotacoes

--- src/main/java/br/unisul/pweb/quarta/atleta/repositories/AtletaRepository.java
+++ src/main/java/br/unisul/pweb/quarta/atleta/repositories/AtletaRepository.java
@@ -1,9 +1,11 @@
 package br.unisul.pweb.quarta.atleta.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.unisul.pweb.quarta.atleta.domain.Atleta;
 
+@Repository
 public interface AtletaRepository extends JpaRepository<Atleta, Integer>{
 
 }
--- src/main/java/br/unisul/pweb/quarta/atleta/repositories/CompeticaoRepository.java
+++ src/main/java/br/unisul/pweb/quarta/atleta/repositories/CompeticaoRepository.java
@@ -1,9 +1,11 @@
 package br.unisul.pweb.quarta.atleta.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.unisul.pweb.quarta.atleta.domain.Competicao;;
 
+@Repository
 public interface CompeticaoRepository extends JpaRepository<Competicao, Integer>{
 
 }
